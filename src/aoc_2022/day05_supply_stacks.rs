5b16b746a3349b2cc7ff5c2b65aa75af80707aee
Mota Link <kerz9966@foxmail.com>
2022-12-05 17:13:22 +0800

    Add: AoC 2022 Day 05

---
+++ src/aoc_2022/day05_supply_stacks.rs
@@ -0,0 +1,92 @@
+use std::fs::read_to_string;
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref STACKS: Vec<Vec<char>> = vec![
+        vec!['S', 'T', 'H', 'F', 'W', 'R'],
+        vec!['S', 'G', 'D', 'Q', 'W'],
+        vec!['B', 'T', 'W'],
+        vec!['D', 'R', 'W', 'T', 'N', 'Q', 'Z', 'J'],
+        vec!['F', 'B', 'H', 'G', 'L', 'V', 'T', 'Z'],
+        vec!['L', 'P', 'T', 'C', 'V', 'B', 'S', 'G'],
+        vec!['Z', 'B', 'R', 'T', 'W', 'G', 'P'],
+        vec!['N', 'G', 'M', 'T', 'C', 'J', 'R'],
+        vec!['L', 'G', 'B', 'W'],
+    ];
+}
+
+fn part_1(input: &str, mut stacks: Vec<Vec<char>>) -> String {
+    input
+        .lines()
+        .map(|i| parser(i))
+        .for_each(|(mov, from, to)| {
+            for _ in 0..mov {
+                let ch = stacks[from - 1].pop().unwrap();
+                stacks[to - 1].push(ch);
+            }
+        });
+
+    get_stacks_top(stacks)
+}
+
+fn part_2(input: &str, mut stacks: Vec<Vec<char>>) -> String {
+    input
+        .lines()
+        .map(|i| parser(i))
+        .for_each(|(mov, from, to)| {
+            let boundary = stacks[from - 1].len() - mov;
+            let mut drain = stacks[from - 1].drain(boundary..).collect();
+            stacks[to - 1].append(&mut drain)
+        });
+
+    get_stacks_top(stacks)
+}
+
+fn parser(input: &str) -> (usize, usize, usize) {
+    let mut nums = input
+        .split_whitespace()
+        .filter_map(|item| item.parse::<usize>().ok());
+
+    (
+        nums.next().unwrap(),
+        nums.next().unwrap(),
+        nums.next().unwrap(),
+    )
+}
+
+fn get_stacks_top(stacks: Vec<Vec<char>>) -> String {
+    stacks
+        .into_iter()
+        .map(|stack| *stack.last().unwrap())
+        .collect()
+}
+
+pub fn day05_answer() {
+    let input = read_to_string("./src/aoc_2022/input/input_2022_05").unwrap();
+    println!(
+        "Part 1: {}\nPart 2: {}",
+        part_1(&input.trim(), STACKS.clone()),
+        part_2(&input.trim(), STACKS.clone())
+    )
+}
+
+#[test]
+fn part_1_test() {
+    let input: &str = "move 1 from 2 to 1\n\
+                       move 3 from 1 to 3\n\
+                       move 2 from 2 to 1\n\
+                       move 1 from 1 to 2";
+    let stacks = vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']];
+    assert_eq!(part_1(input, stacks), "CMZ");
+}
+
+#[test]
+fn part_2_test() {
+    let input: &str = "move 1 from 2 to 1\n\
+                       move 3 from 1 to 3\n\
+                       move 2 from 2 to 1\n\
+                       move 1 from 1 to 2";
+    let stacks = vec![vec!['Z', 'N'], vec!['M', 'C', 'D'], vec!['P']];
+    assert_eq!(part_2(input, stacks), "MCD");
+}
--- src/aoc_2022/mod.rs
+++ src/aoc_2022/mod.rs
@@ -1,3 +1,4 @@
 pub mod day02_rock_paper_scissors;
 pub mod day03_rucksack_reorganization;
 pub mod day04_camp_cleanup;
+pub mod day05_supply_stacks;
