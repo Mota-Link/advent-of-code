c8d8e1ef622b7b910bbf6bd34fbdfbd949055503
Mota Link <kerz9966@foxmail.com>
2022-12-05 23:17:19 +0800

    Add: AoC 2021 Day 01

---
+++ src/aoc_2021/day01_sonar_sweep.rs
@@ -0,0 +1,63 @@
+use std::fs::read_to_string;
+
+fn part_1(input: &str) -> usize {
+    input
+        .lines()
+        .map(|depth| depth.parse::<u32>().unwrap())
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|couple| couple[0] < couple[1])
+        .count()
+}
+
+fn part_2(input: &str) -> usize {
+    input
+        .lines()
+        .map(|depth| depth.parse::<u32>().unwrap())
+        .collect::<Vec<_>>()
+        .windows(3)
+        .map(|window| window.into_iter().sum::<u32>())
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|couple| couple[0] < couple[1])
+        .count()
+}
+
+pub fn day01_answer() {
+    let input = read_to_string("./src/aoc_2021/input/input_2021_01").unwrap();
+    println!(
+        "Part 1: {}\nPart 2: {}",
+        part_1(&input.trim()),
+        part_2(&input.trim())
+    )
+}
+
+#[test]
+fn part_1_test() {
+    let input: &str = "199\n\
+                       200\n\
+                       208\n\
+                       210\n\
+                       200\n\
+                       207\n\
+                       240\n\
+                       269\n\
+                       260\n\
+                       263";
+    assert_eq!(part_1(input), 7);
+}
+
+#[test]
+fn part_2_test() {
+    let input: &str = "199\n\
+                       200\n\
+                       208\n\
+                       210\n\
+                       200\n\
+                       207\n\
+                       240\n\
+                       269\n\
+                       260\n\
+                       263";
+    assert_eq!(part_2(input), 5);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
 pub mod aoc_2022;
+pub mod aoc_2021;
